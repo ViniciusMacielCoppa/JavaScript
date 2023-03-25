c8d5e3f1ee1302f847ac251de7ba9959f8383097
vinicius coppa <viniciuscoppa@gmail.com>
2023-03-24 22:34:16 -0300

    versão final

--- LEMBRAR DE MIM - JavaScript/assets/scripts/script.js
+++ LEMBRAR DE MIM - JavaScript/assets/scripts/script.js
@@ -1,58 +1,60 @@
 
-const botao = document.querySelector('#btnlembrar')
 let User_email = document.querySelector('#userEmail')
 let User_senha = document.querySelector('#userSenha')
+const botao = document.querySelector('#btnlembrar')
+
+let numero = 0
 
 if(typeof(Storage) != 'undefined'){
 
-	if(localStorage.userEmail){
-		User_email.value = localStorage.userEmail
-	}
-	if(localStorage.userSenha){
-		User_senha.value = localStorage.userSenha
+	if(!localStorage.save){
+		localStorage.save = 'nao'
+		botao.innerText = 'Lembrar de mim'
+	}else if(localStorage.save == 'sim'){
+		if(localStorage.Senha){
+			User_senha.value = localStorage.Senha
+		}
+		if(localStorage.Email){
+			User_email.value = localStorage.Email
+		}
+		botao.innerText = 'Reconsiderar'
 	}
 
 	botao.addEventListener('click', ()=> {
-		if(checkbox.checked){
-			localStorage.save = true
 
+		if(localStorage.getItem('save') == 'nao'){
 			if(User_email.value != ''){
-				localStorage.userEmail = User_email.value
-			}else{
-				localStorage.removeItem('userEmail')
+				localStorage.Email = User_email.value
 			}
 			if(User_senha.value != ''){
-				localStorage.userSenha = User_senha.value
-			}else{
-				localStorage.removeItem('userSenha')
+				localStorage.Senha = User_senha.value
 			}
-		}else{
-			localStorage.clear()
-			localStorage.save = false
+
+			localStorage.save = 'sim'
+			botao.innerText = 'Reconsiderar'
+			return
 		}
+			botao.innerText = 'Lembrar de mim'
+			localStorage.save = 'nao'
+			localStorage.removeItem('Senha')
+			localStorage.removeItem('Email')
 	})
 
-	if(!localStorage.getItem('save')){
-		User_email.addEventListener('input', ()=>{
-			if(User_email.value != ''){
-				localStorage.userEmail = User_email.value
-			}else{
-				localStorage.removeItem('userEmail')
-			}
-		})
-		User_senha.addEventListener('input', ()=>{
-			if(User_senha.value != null){
-				localStorage.userSenha = User_senha.value
-			}
-			else{
-				localStorage.removeItem('userSenha')
-			}
-		})
-	}
+	User_email.addEventListener('input', ()=> {
+		if(localStorage.save == 'sim' && User_email.value != ''){
+			localStorage.Email = User_email.value
+			return
+		}
+		localStorage.removeItem('Email')
+	})
+	User_senha.addEventListener('input', ()=> {
+		if(localStorage.save == 'sim' && User_senha.value != ''){
+			localStorage.Senha = User_senha.value
+			return
+		}
+		localStorage.removeItem('Senha')
+	})
 
 }else{
   alert('Este navegador não possui suporte para Cookies!')
 }
-
-
-
