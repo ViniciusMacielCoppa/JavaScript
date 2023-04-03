ac3624cdbcd343c040d9df1b3736e01f4cccaf5d
vinicius coppa <viniciuscoppa@gmail.com>
2023-04-02 21:45:24 -0300

    att array e check/uncheck em tarefas

--- TO DO LIST - JavaScript/assets/js/script.js
+++ TO DO LIST - JavaScript/assets/js/script.js
@@ -2,34 +2,41 @@ const escreverTarefa = document.querySelector('#f-addTarefa')
 const btnAdd = document.querySelector('#btn-addTarefa')
 const lista = document.querySelector('ul')
 
-const tarefas = [
-	{
-		name: 'Aprender muito'
-	}
-]
+const tarefas = []
 
 MostrarTarefas()
 
 function MostrarTarefas(){
 	let conter = ''
-	if(tarefas.length > 0){
-		for(let i = 0; i < tarefas.length; i++){
-			conter += `
-		        <li>
-		          <p class='finalizada${i}'>${tarefas[i].name}</p>
-		          <div>
-		            <button id='btn-check' onclick='MarcarFinalizado(${i})'><i class="fa-solid fa-check"></i></button>
-		            <button id='btn-delete' onclick='DeletarTarefa(${i})'><i class="fa-solid fa-trash"></i></button>
-		          </div>
-		        </li>
-			`
-		}
+
+	if(tarefas.length < 1){
+    lista.innerHTML = "<li id='semTarefas'>Não há tarefas registradas</li>"
+    return
+  	}
+
+	for(let i = 0; i < tarefas.length; i++){
+		conter += 
+    `
+    <li>
+      <p ${tarefas[i].checado == true ? "class='checado'" : "class=''"}>
+        ${tarefas[i].name}
+      </p>
+      <div>
+        <button id='btn-check' onclick='MarcarFinalizado(${i})'>
+          ${tarefas[i].checado == true ? "<i class='fa-solid fa-xmark'></i>" : "<i class='fa-solid fa-check'></i>"}
+        </button>
+        <button id='btn-delete' onclick='DeletarTarefa(${i})'><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </li>
+	`
 	}
+
 	lista.innerHTML = conter
 }
 
 function MarcarFinalizado(i){
-	document.querySelector(`.finalizada${i}`).style.textDecoration = 'line-through'
+ 	tarefas[i].checado === false ? tarefas[i].checado = true : tarefas[i].checado = false
+  	MostrarTarefas()
 }
 
 function DeletarTarefa(i){
@@ -39,12 +46,14 @@ function DeletarTarefa(i){
 
 btnAdd.addEventListener('click', ()=> {
 	let novaTarefa = {
-		name: ''
+		name: '',
+		checado: false
 	}
 
 	if(escreverTarefa.value != '' && escreverTarefa.value != ' '){
 		document.querySelector('.tarefaadd').style.opacity = '1';
 		document.querySelector('.tarefaadd').style.top = '30px';
+
 		setTimeout(()=> {
 			document.querySelector('.tarefaadd').style.opacity = '0';
 			document.querySelector('.tarefaadd').style.top = '0px';
@@ -57,12 +66,15 @@ btnAdd.addEventListener('click', ()=> {
 		MostrarTarefas()
 		return
 	}
+
 	document.querySelector('.tarefaerro').style.opacity = '1';
 	document.querySelector('.tarefaerro').style.top = '30px';
+
 	setTimeout(()=> {
 		document.querySelector('.tarefaerro').style.opacity = '0';
 		document.querySelector('.tarefaerro').style.top = '0px';
 	},1000)
+	
 	escreverTarefa.value = ''
 	escreverTarefa.focus()
 })
